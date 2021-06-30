dab6ea9da005092cbeab9a4914df722a7b88c27c
agnese-web <agnesefolkberga@gmail.com>
2021-06-30 18:07:59 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String five = "5";
+	String eight = "8";
+	int five5 = Integer.parseInt(five);
+	int eight8 = Integer.parseInt(eight);
+	int sum = five5 + eight8;
+        System.out.println("The sum is " + sum);
+    }
+}
